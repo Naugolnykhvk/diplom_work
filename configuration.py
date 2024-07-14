f4989de742fe3b515f6ccd145dbc4ce1f3c450ac
Naugolnykhvk <Larkova-viktoriya@mail.ru>
2024-07-14 17:43:18 +0500

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Наугольных Виктория, 18 когорта, Инженер по тестированию плюс
+
+URL_SERVICE = "https://ee8593fe-7e0e-457e-bf3f-a3c4a0c0832a.serverhub.praktikum-services.ru/"
+CREAT_ORDERS = "/api/v1/orders"
